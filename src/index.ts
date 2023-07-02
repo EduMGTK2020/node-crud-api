8a7bdf8664dd60b5837308f5493162e6bb3a931e
Ed <mail@544062.ru>
2023-07-02 11:50:38 +0300

    feat: add simple http server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log('Hello world! Nodemon, Webpack')
+import { createServer, IncomingMessage, ServerResponse } from 'http';
+
+const port = 4000;
+
+const server = createServer(
+  (request: IncomingMessage, response: ServerResponse) => {
+    console.log(request.url);
+    response.end('Hello world!');
+  }
+);
+
+server.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
